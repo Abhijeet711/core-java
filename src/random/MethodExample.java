ae160c00c64976c0cc966435b687b734d8b66eb0
Abhijeet Bagwe <84623391+Abhijeet711@users.noreply.github.com>
2023-04-16 14:01:49 +0530

    initial commit

--- src/random/MethodExample.java
+++ src/random/MethodExample.java
@@ -10,12 +10,17 @@ package random;
  *
  * @author Abhi
  */
-public class ObjectMethod {
+public class MethodExample {
+    //in methods, if an object is passed its value of actual object will be modified, but if the datatype of that object is primitive, the value won't be modified.
     static void update(int A[]){
         A[2] = 7;
     }
-    
-    //in methods, if an object is passed its value of actual object will be modified, but if the datatype of that object is primitive, the value won't be modified wh.
+    //return type of a method. (String return type not working, still trying to fix. the code works with void return type.)
+    public static String username(String email){
+        int a = email.indexOf('@');
+        String uname = email.substring(0,a);
+        return uname;
+    }
     public static void main(String[] args) {
         int A[] = {1,2,3,4,5};
         System.out.print("Array before update(): ");
@@ -28,5 +33,9 @@ public class ObjectMethod {
         for(int x:A){
             System.out.print(x + " ");
         }
+        System.out.println("\n\n--EXAMPLE OVER--\n");        
+        String email = "abhijeet.b@somaiya.edu";
+        System.out.print("Username is: ");
+        username(email);
     }
 }
